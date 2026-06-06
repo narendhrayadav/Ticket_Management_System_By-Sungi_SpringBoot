11aad9d45f0628b10d4eb115bff3ea722dc9dcef
narendhrayadav <142585695+narendhrayadav@users.noreply.github.com>
2026-06-06 17:43:46 +0530

    Create FeatureTicketDTO.java

---
+++ FeatureTicketDTO.java
@@ -0,0 +1,14 @@
+package com.example.ticketmanagement.dto;
+
+public class FeatureTicketDTO extends TicketDTO {
+
+    private String featureName;
+
+    public String getFeatureName() {
+        return featureName;
+    }
+
+    public void setFeatureName(String featureName) {
+        this.featureName = featureName;
+    }
+}
